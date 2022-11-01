add5c3314b2e83c4f2967054dcc01c692b874b32
Hung <hung@usaepay.com>
2022-11-01 04:04:46 -0700

    added new mongo pass

--- components/ConnectButton.js
+++ components/ConnectButton.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import { ConnectButton } from 'web3uikit';
+import React from "react";
+import { ConnectButton } from "web3uikit";
 
 const ConnectButton2 = () => {
-    return ( <div> <ConnectButton moralisAuth={false}/> </div> );
-}
+  return (
+    <div>
+      <ConnectButton moralisAuth={false} />
+    </div>
+  );
+};
 
 export default ConnectButton2;
--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,31 +1,31 @@
 const main = async () => {
-  // const btFactory = await hre.ethers.getContractFactory('BitHo')
-  // const btContract = await btFactory.deploy()
-  // await btContract.deployed()
-  // console.log('BitHo deployed to:', btContract.address)
+    // const btFactory = await hre.ethers.getContractFactory('BitHo')
+    // const btContract = await btFactory.deploy()
+    // await btContract.deployed()
+    // console.log('BitHo deployed to:', btContract.address)
 
-  // const bitFactory = await hre.ethers.getContractFactory('BitTo')
-  // const bitContract = await bitFactory.deploy()
-  // await bitContract.deployed()
-  // console.log('BitTo deployed to:', bitContract.address)
+    // const bitFactory = await hre.ethers.getContractFactory('BitTo')
+    // const bitContract = await bitFactory.deploy()
+    // await bitContract.deployed()
+    // console.log('BitTo deployed to:', bitContract.address)
 
-  // const flFactory = await hre.ethers.getContractFactory('FLuna')
-  // const flContract = await flFactory.deploy()
-  // await flContract.deployed()
-  // console.log('Fluna deployed to:', flContract.address)
+    // const flFactory = await hre.ethers.getContractFactory('FLuna')
+    // const flContract = await flFactory.deploy()
+    // await flContract.deployed()
+    // console.log('Fluna deployed to:', flContract.address)
 
-  const scFactory = await hre.ethers.getContractFactory('Scoin')
-  const scContract = await scFactory.deploy()
-  await scContract.deployed()
-  console.log('Scoin deployed to:', scContract.address)
+    const scFactory = await hre.ethers.getContractFactory("Scoin")
+    const scContract = await scFactory.deploy()
+    await scContract.deployed()
+    console.log("Scoin deployed to:", scContract.address)
 }
 
-  ; (async () => {
-      try {
-          await main()
-          process.exit(0)
-      } catch (error) {
-          console.error(error)  
-          process.exit(1)
-      }
-  })()
+;(async () => {
+    try {
+        await main()
+        process.exit(0)
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+})()
